b161654db016c6610e729144dd56eb0f74a5ddbe
Jacob Linteau <jacob.linteau@hotmail.com>
2023-08-05 14:28:47 -0400

    Ajout de l'authentification pour twitch

--- APIFunctions.js
+++ APIFunctions.js
@@ -1,8 +1,26 @@
-function GetTiktokInfos() {
-    fetch('https://open.tiktokapis.com/v2/user/info', {
+function GetTwitchClips(token) {
+    fetch('https://api.twitch.tv/helix/clips', {
         method: 'get',
         headers: {
             
         }
     })
+}
+
+function AuthTwitch() {
+    fetch('https://id.twitch.tv/oauth2/token', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            client_id: 'r4qo7fi9jgkak7s3xxnggz9o9ca0mn',
+            client_secret: 'cg7z363ikkvf1ocrk8f2c5osmaws45',
+            grant_type: 'client_credentials'
+        })
+    }).then(res=> {
+            return res.json()
+        })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
 }
