5650f911100e45b584eae4d6325459b32f7c9fe7
Jacob Linteau <jacob.linteau@hotmail.com>
2023-08-05 16:06:12 -0400

    Fix quand on return la valeur du token

--- APIFunctions.js
+++ APIFunctions.js
@@ -1,3 +1,28 @@
+function AuthTwitch() {
+    var token
+    fetch('https://id.twitch.tv/oauth2/token', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            client_id: 'r4qo7fi9jgkak7s3xxnggz9o9ca0mn',
+            client_secret: 'cg7z363ikkvf1ocrk8f2c5osmaws45',
+            grant_type: 'client_credentials'
+        })
+    }).then(res => {
+        return res.json()
+    })
+    .then((data) => {
+        console.log(data)
+        token = data.access_token
+        console.log(token)
+    })
+    .catch(error => console.log(error))
+
+    return token
+}
+
 function GetTwitchClips(token) {
     fetch('https://api.twitch.tv/helix/clips', {
         method: 'get',
@@ -9,24 +34,6 @@ function GetTwitchClips(token) {
         body: JSON.stringify({
             broadcaster_id: '474447255',
             first: 5
-        })
-    }).then(res => {
-        return res.json()
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-}
-
-function AuthTwitch() {
-    fetch('https://id.twitch.tv/oauth2/token', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            client_id: 'r4qo7fi9jgkak7s3xxnggz9o9ca0mn',
-            client_secret: 'cg7z363ikkvf1ocrk8f2c5osmaws45',
-            grant_type: 'client_credentials'
         })
     }).then(res => {
         return res.json()
